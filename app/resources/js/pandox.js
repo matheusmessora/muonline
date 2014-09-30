ca40065aaaec10b30b52325ada25ab703452cb27
Matheus Messora <tqi_mmessora@uolinc.com>
2014-09-29 21:17:46 -0300

    melhoria loja

--- app/resources/js/pandox.js
+++ app/resources/js/pandox.js
@@ -233,9 +233,9 @@ PANDOX.SHOP = function () {
             var itemId = $(this).attr('x-item-id');
             console.log(itemId);
 
-            $("#shop-display").fadeOut();
+            $("#shop-display").hide();
 
-            $("#shop-cart").show();
+            $("#shop-cart").fadeIn();
 
             //            $.getJSON("/shop/includes/sword.js", {})
             //                .done(function (json) {
