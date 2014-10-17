aa7d65505f36cd119d5409ecb9e8f706d9b4a031
Matheus Messora <matheus.messora.vpn@gmail.com>
2014-10-17 02:53:01 -0300

    launcher

--- launcher/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ launcher/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApplication1
+namespace WindowsFormsApplication1
 {
     partial class Form1
     {
@@ -28,47 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.label1 = new System.Windows.Forms.Label();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.radio1280 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.launcher_btn_start;
+            this.button1.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.launcher_btn_start1;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.ForeColor = System.Drawing.Color.Transparent;
-            this.button1.Location = new System.Drawing.Point(553, 365);
+            this.button1.Location = new System.Drawing.Point(645, 354);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(219, 53);
+            this.button1.Size = new System.Drawing.Size(127, 57);
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.BackColor = System.Drawing.Color.Transparent;
-            this.radioButton2.ForeColor = System.Drawing.Color.White;
-            this.radioButton2.Location = new System.Drawing.Point(128, 401);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(72, 17);
-            this.radioButton2.TabIndex = 2;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "1024x768";
-            this.radioButton2.UseVisualStyleBackColor = false;
-            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
-            // 
+            //
             // webBrowser1
             // 
             this.webBrowser1.Location = new System.Drawing.Point(39, 135);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(733, 224);
+            this.webBrowser1.Size = new System.Drawing.Size(733, 193);
             this.webBrowser1.TabIndex = 3;
             this.webBrowser1.Url = new System.Uri("http://www.worldofmu.com.br/launcher", System.UriKind.Absolute);
             // 
@@ -81,21 +77,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(51, 13);
             this.label1.TabIndex = 4;
-            this.label1.Text = "0.0.1-alfa";
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.BackColor = System.Drawing.Color.Transparent;
-            this.radioButton1.ForeColor = System.Drawing.Color.White;
-            this.radioButton1.Location = new System.Drawing.Point(56, 401);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(66, 17);
-            this.radioButton1.TabIndex = 1;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "800x600";
-            this.radioButton1.UseVisualStyleBackColor = false;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            this.label1.Text = "0.0.2-alfa";
             // 
             // button2
             // 
@@ -111,36 +93,110 @@
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // checkBox1
+            //
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(299, 378);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(77, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Tela Cheia";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            //
+            // groupBox1
+            //
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox1.Controls.Add(this.radio1280);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.Info;
+            this.groupBox1.Location = new System.Drawing.Point(39, 354);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(254, 61);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Resolução";
+            //
             // radio1280
             // 
             this.radio1280.AutoSize = true;
             this.radio1280.BackColor = System.Drawing.Color.Transparent;
             this.radio1280.ForeColor = System.Drawing.Color.White;
-            this.radio1280.Location = new System.Drawing.Point(206, 401);
+            this.radio1280.Location = new System.Drawing.Point(170, 24);
             this.radio1280.Name = "radio1280";
-            this.radio1280.Size = new System.Drawing.Size(78, 17);
-            this.radio1280.TabIndex = 6;
+            this.radio1280.Size = new System.Drawing.Size(72, 17);
+            this.radio1280.TabIndex = 9;
             this.radio1280.TabStop = true;
-            this.radio1280.Text = "1600x1024";
+            this.radio1280.Text = "1024x768";
             this.radio1280.UseVisualStyleBackColor = false;
-            this.radio1280.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            //
+            // radioButton2
+            //
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton2.ForeColor = System.Drawing.Color.White;
+            this.radioButton2.Location = new System.Drawing.Point(92, 24);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(66, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "800x600";
+            this.radioButton2.UseVisualStyleBackColor = false;
+            //
+            // radioButton1
+            //
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton1.ForeColor = System.Drawing.Color.White;
+            this.radioButton1.Location = new System.Drawing.Point(20, 24);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(66, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "640x480";
+            this.radioButton1.UseVisualStyleBackColor = false;
+            //
+            // linkLabel1
+            //
+            this.linkLabel1.ActiveLinkColor = System.Drawing.SystemColors.ActiveCaption;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel1.ForeColor = System.Drawing.Color.White;
+            this.linkLabel1.LinkColor = System.Drawing.Color.White;
+            this.linkLabel1.Location = new System.Drawing.Point(36, 459);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(117, 13);
+            this.linkLabel1.TabIndex = 10;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "www.worldofmu.com.br";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.launcher_bg1;
-            this.ClientSize = new System.Drawing.Size(802, 481);
-            this.Controls.Add(this.radio1280);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(800, 480);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.webBrowser1);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "World of MU - Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -149,12 +205,15 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.WebBrowser webBrowser1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RadioButton radio1280;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
 
--- launcher/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ launcher/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
 using Microsoft.Win32;
+using System.Security.Principal;
+using System.IO;
 
 namespace WindowsFormsApplication1
 {
@@ -21,13 +23,13 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            setMinimized();
+
             Process.Start("main.exe");
         }
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
-            addResolution(2);
+            addResolution(1);
         }
 
         private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
@@ -43,27 +45,49 @@ namespace WindowsFormsApplication1
 
         private void radioButton3_CheckedChanged(object sender, EventArgs e)
         {
-            addResolution(4);
+            addResolution(3);
         }
 
-        private void setMinimized()
+        private static bool IsAdministrator()
+        {
+            return (new WindowsPrincipal(WindowsIdentity.GetCurrent())).IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        private void setWindowMode(Boolean minimized)
         {
             RegistryKey reg = Registry.CurrentUser;
             RegistryKey subKey = reg.OpenSubKey(@"Software\Webzen\MU\Config", true);
 
+            int valor = minimized ? 1 : 0;
+
             if (subKey == null)
             {
                 RegistryKey newKey = reg.CreateSubKey(@"Software\Webzen\MU\Config");
-                newKey.SetValue("WindowMode", 1, RegistryValueKind.DWord);
+                newKey.SetValue("WindowMode", valor, RegistryValueKind.DWord);
                 newKey.Close();
             }
             else
             {
-                subKey.SetValue("WindowMode", 1, RegistryValueKind.DWord);
+                subKey.SetValue("WindowMode", valor, RegistryValueKind.DWord);
                 subKey.Close();
             }
         }
 
+        private void removeID()
+        {
+            RegistryKey reg = Registry.CurrentUser;
+            RegistryKey subKey = reg.OpenSubKey(@"Software\Webzen\MU\Config", true);
+
+            if (subKey != null)
+            {
+                Object id = subKey.GetValue("ID");
+                if (id != null)
+                {
+                    subKey.DeleteValue("ID");
+                }
+
+            }
+        }
         private void addResolution(int id)
         {
             RegistryKey reg = Registry.CurrentUser;
@@ -84,7 +108,25 @@ namespace WindowsFormsApplication1
 
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
-            addResolution(3);
+            addResolution(2);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            setWindowMode(!checkBox1.Checked);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+                setWindowMode(false);
+                radio1280.Checked = true;
+                checkBox1.Checked = true;
+                addResolution(3);
+                removeID();
+
+
+
         }
     }
 }
